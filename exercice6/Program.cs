8a000305280c7a34d257f479fb70ad5b2fd9dfbc
BakariF <bakkfofhit@live.fr>
2021-01-15 16:48:21 +0100

    secondary

--- exercice6/Program.cs
+++ exercice6/Program.cs
@@ -5,8 +5,18 @@ namespace exercice6
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {    
+            Console.WriteLine("nom");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Veuillez écrire votre prénom sur ce magnifique input");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("How old are you ?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fiche de renseignement");
+            Console.WriteLine("Nom : " + lastName);
+            Console.WriteLine("Prénom : " + firstName);
+            Console.WriteLine("Année de naissance : " + age + "ans");
+
         }
     }
 }
